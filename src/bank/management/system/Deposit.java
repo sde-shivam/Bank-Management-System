a8a81dfff396ce7641da2b886e1dc60619d75a66
Shivam Singh <shivamasingh.tcsc@gmail.com>
2025-12-28 10:54:53 +0530

    add deposit and back button and added ActionListner

--- src/bank/management/system/Deposit.java
+++ src/bank/management/system/Deposit.java
@@ -1,11 +1,16 @@
 package bank.management.system;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Deposit extends JFrame {
-    Deposit(){
-        
+public class Deposit extends JFrame implements ActionListener {
+    JTextField amount;
+    JButton deposit,back;
+    String pinnumber;
+    
+    Deposit(String pinnumber){
+      this.pinnumber=pinnumber;
       setLayout(null);  
         
       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
@@ -21,13 +26,36 @@ public class Deposit extends JFrame {
       text.setBounds(170, 300, 400, 20);
       image.add(text);
       
+      amount = new JTextField();
+      amount.setFont(new Font("Raleway",Font.BOLD,22));
+      amount.setBounds(170,350,320,25);
+      image.add(amount);
       
+      deposit = new JButton("Deposit");
+      deposit.setBounds(355,485,150,30);
+      deposit.addActionListener(this);
+      image.add(deposit);
+      
+      back = new JButton("Back");
+      back.setBounds(355,520,150,30);
+      back.addActionListener(this);
+      image.add(back);
       
         setSize(900, 900);
         setLocation(100,0);
         setVisible(true);
     }
+    
+    public void actionPerformed(ActionEvent ae){
+     if(ae.getSource()== deposit){
+     
+     }else if(ae.getSource()==back){
+         setVisible(false);
+         new Transactions(pinnumber).setVisible(true);
+     }
+    }
+    
     public static void main(String args[]){
-    new Deposit();
+    new Deposit("");
    } 
 }
--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -68,7 +68,10 @@ public class Transactions extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent ae){
     if(ae.getSource()== exit){
         System.exit(0);
-    }
+      }else if(ae.getSource()==deposit){
+       setVisible(false);
+       new Deposit(pinnumber).setVisible(true);
+      }
     }
     public static void main(String args[]) {
        new Transactions("");
