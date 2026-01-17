754231765f01567a5fbd10f582fb579f40afcaab
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-01-17 23:29:57 +0530

    Ministatement class completed

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -83,7 +83,9 @@ public class Transactions extends JFrame implements ActionListener{
       }else if(ae.getSource()==balanceenquiry){
        setVisible(false);
        new BalanceEnquiry(pinnumber).setVisible(true);
-    }
+    }else if(ae.getSource()==ministatement){
+       new MiniStatement(pinnumber).setVisible(true);
+     }  
     }
      public static void main(String args[]) {
        new Transactions("");
